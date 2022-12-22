88b8c11d68c1469fc82bc96fc7962f0062e1c091
ZL-Marcos <87494218+zl-marcos@users.noreply.github.com>
2022-12-22 17:25:47 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,8 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'ZL-Marcos'
-description 'Pack de scripts utiles'
+description 'Pack of scripts that are useful ;)'
 lua54 'yes'
-version '1.0'
+version '1.1'
 
 client_script 'client/*.lua'
